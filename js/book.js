411cacc91d02f11912bce9a43ccf01f181d08f02
SirriCelles <sirricelles@gmail.com>
2022-08-01 12:26:26 +0100

    update Book Class

--- js/book.js
+++ js/book.js
@@ -1,21 +1,37 @@
 class Book {
     booksArray = [];
 
-    constructor(book) {
-      this.book = book;
+    get getBooks() {
+      if (this.checkBook) {
+        const localBook = localStorage.getItem('books');
+        this.booksArray = JSON.parse(localBook);
+        return this.booksArray;
+      }
+      return this.booksArray;
     }
 
-    addBook() {
-      if (this.book) {
+    // eslint-disable-next-line class-methods-use-this
+    checkBook() {
+      return localStorage.getItem('books') !== null;
+    }
+
+    updateBooks() {
+      localStorage.setItem('books', this.booksArray);
+    }
+
+    addBook(book) {
+      if (book) {
         this.booksArray.push(this.book);
+        this.updateBooks();
       }
     }
 
-    removeBook() {
+    removeBook(book) {
       const oldBook = [...this.booksArray];
-      if (this.book) {
+      if (book) {
         this.booksArray = oldBook
           .filter((el) => el.title !== this.book.title && el.name !== this.book.author);
+        this.updateBooks();
       }
     }
 }
